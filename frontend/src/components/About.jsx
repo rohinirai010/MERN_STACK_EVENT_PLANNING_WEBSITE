42f5135b9cdb16bad4f3670fa867327a8ea3c8e4
rohinirai010 <rairohini2001@gmail.com>
2024-07-10 18:14:18 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+        modi molestias excepturi iste suscipit asperiores maxime corporis! Nihil
+        distinctio neque consectetur. Veritatis totam, labore ex adipisci
+        impedit vel fugiat quia qui, nesciunt reiciendis natus nisi cumque quod
+        sapiente doloremque! Consectetur reprehenderit omnis, nobis nulla
+        necessitatibus debitis, impedit ullam ab veritatis, laudantium animi
+        quia! Repudiandae ea quo, tempore praesentium accusamus ratione, dicta,
+        quibusdam nobis voluptatem sed nesciunt facere nemo iure. Modi fuga
+        perspiciatis, libero laudantium quidem iure maxime quisquam aperiam
+        velit est hic voluptatem quis facilis doloremque ex! Ex iusto fuga,
+        nostrum facere voluptate dolor vero in, inventore obcaecati, pariatur
+        nesciunt.
+      </p>
+    </section>
+  );
+};
+
+export default About;
