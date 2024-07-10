75b5c5c514e8d4a299c19ea0edda27db9096c677
rohinirai010 <rairohini2001@gmail.com>
2024-07-10 19:36:19 +0530

    sixth commit

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,36 @@
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import dotenv from "dotenv";
-import messageRouter from "./router/messageRouter.js"
+import messageRouter from "./router/messageRouter.js";
 import cors from "cors";
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-// Initialize express app
 const app = express();
 
-// Load environment variables from config.env file
-dotenv.config({path: "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
-// Configure CORS
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"], 
-    credentials: true,
+  origin: [process.env.FRONTEND_URL],
+  methods: ["POST"],
+  credentials: true,
 }));
 
-// Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-  // Mount message router
-app.use("/api/v1/message",messageRouter); 
+app.use("/api/v1/message", messageRouter);
 
 dbConnection();
+
+// Proxy requests to Vercel frontend
+const vercelUrl = process.env.FRONTEND_URL;
+
+app.use('/*', createProxyMiddleware({
+  target: vercelUrl,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/': '/', // remove base path
+  },
+}));
 
 export default app;
