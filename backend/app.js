fddb4548bd0fcf217382fd088cde40dc6a7c9e98
rohinirai010 <rairohini2001@gmail.com>
2024-07-10 19:08:44 +0530

    fourth commit

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,29 @@ import dotenv from "dotenv";
 import messageRouter from "./router/messageRouter.js"
 import cors from "cors";
 
+// Initialize express app
 const app = express();
 
+// Load environment variables from config.env file
 dotenv.config({path: "./config/config.env"});
 
+// Configure CORS
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["POST"], 
     credentials: true,
 }));
 
+// Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// Root route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+
+  // Mount message router
 app.use("/api/v1/message",messageRouter); 
 
 dbConnection();
